dc6727b7a068e92911b10911f2033e3f6d036377
Kevin van Gils <kvnvangils@gmail.com>
2022-03-31 11:01:38 +0200

    start of project

---
+++ app/src/androidTest/java/com/example/pahteapp/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.example.pahteapp;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.pahteapp", appContext.getPackageName());
+    }
+}
