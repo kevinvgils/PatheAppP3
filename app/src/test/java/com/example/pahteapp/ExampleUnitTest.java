ded13d74a4e3268d6c5d0fb81ed9cf7416fdfea9
Ha-Satan <47351839+Ha-Satan@users.noreply.github.com>
2022-04-06 22:12:51 +0200

    Made tests for movie calculations

--- app/src/test/java/com/example/pahteapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/pahteapp/ExampleUnitTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.example.pahteapp.domain.Movie;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -14,4 +16,44 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void Movieruntime_convertedToHoursAndMinutes(){
+        Movie movie = new Movie();
+        movie.setRuntime(148);
+
+        assertEquals(movie.getRuntime() / 60 + "h" + movie.getRuntime() % 60 + "m" ,"2h28m");
+    }
+    @Test
+    public void Movieruntime_convertedToHoursAndMinutes2(){
+        Movie movie = new Movie();
+        movie.setRuntime(180);
+
+        assertEquals(movie.getRuntime() / 60 + "h" + movie.getRuntime() % 60 + "m" ,"3h0m");
+    }
+    @Test
+    public void Movieruntime_convertedToHoursAndMinutes3(){
+        Movie movie = new Movie();
+        movie.setRuntime(239);
+
+        assertEquals(movie.getRuntime() / 60 + "h" + movie.getRuntime() % 60 + "m" ,"3h59m");
+    }
+
+
+    @Test
+    public void MovieVoteAverage_dividedBy2_1(){
+        Movie movie = new Movie();
+        movie.setVoteAverage(8.2);
+
+        assertEquals(movie.getVoteAverage() / 2, 4.1, 0.01);
+    }
+    @Test
+    public void MovieVoteAverage_dividedBy2_2(){
+        Movie movie = new Movie();
+        movie.setVoteAverage(4.0);
+
+        assertEquals(movie.getVoteAverage() / 2, 2, 0.01);
+    }
+
+
 }
